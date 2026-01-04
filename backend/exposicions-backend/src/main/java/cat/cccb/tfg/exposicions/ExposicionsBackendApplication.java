8f08fb636f946ef4170f201cfc411b28e6f08947
BVazquezi <122432367+Bpap9@users.noreply.github.com>
2026-01-04 20:00:36 +0100

    init spring boot backend project

---
+++ backend/exposicions-backend/src/main/java/cat/cccb/tfg/exposicions/ExposicionsBackendApplication.java
@@ -0,0 +1,13 @@
+package cat.cccb.tfg.exposicions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposicionsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExposicionsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/exposicions-backend/src/test/java/cat/cccb/tfg/exposicions/ExposicionsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.cccb.tfg.exposicions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExposicionsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
